58ca67a68cc6189b1c9dea10ae31a78722b25c47
Jimmy <jimminh@163.com>
2020-04-13 18:25:20 +0800

    a b c 三个module 重命名

--- b/docker/ready.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# readiness checker
-
-curl -f localhost:8080/actuator/health
-rv=$?
-exit ${rv}
--- c/docker/liveness.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# liveness.sh checker
-
-# checkout port is open
-nc -zv localhost 8080
-rv=$?
-
-exit ${rv}
